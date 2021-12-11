de6edbb19977afab3772f9eca482a4ca2c7ff36e
DaiVin <vinayakbms@gmail.com>
2021-12-11 21:48:32 +0530

    All updated

--- main.c
+++ main.c
@@ -10,18 +10,15 @@ Initial Security()
  //Sample Code
 }
 
-<<<<<<< HEAD
 feature1()
 {
   //Sample Code for Feature 1
+}
 
-=======
-=======
 feaature2()
 {
   //Sample Code for Feature 2
 }
->>>>>>> feature2
 
 
 }
